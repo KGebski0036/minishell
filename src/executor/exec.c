d12a611d9bffce4cfa54be26b9038c104869452e
Karol Gebski <kgebski@c2s17d101.42wolfsburg.de>
2023-06-13 20:20:09 +0200

    pipes

--- src/executor/exec.c
+++ src/executor/exec.c
@@ -6,7 +6,7 @@
 /*   By: kgebski <kgebski@student.42wolfsburg.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/09 13:23:32 by kgebski           #+#    #+#             */
-/*   Updated: 2023/06/13 18:08:37 by kgebski          ###   ########.fr       */
+/*   Updated: 2023/06/13 18:48:42 by kgebski          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,18 +17,32 @@ int	pc_exec_commands(t_command *commands, t_env *env)
 	int	i;
 
 	i = 0;
-	pc_print_command_table(commands);
 	while (commands[i].command)
 	{
 		if (pipe(commands[i].fd) < 0)
-		{
-			ft_putstr_fd("Filed to create a pipe\n", 2);
-		}
+			pc_quit(env, "Filed to create a pipe\n", 2);
+		i++;
 	}
+	i = 0;
+	while (commands[i].command && commands[i + 1].command)
+	{
+		commands[0].fd[0] = 0;
+		commands[i].fd[1] = commands[i + 1].fd[0];
+		i++;
+	}
+	commands[i].fd[1] = 1;
+	pc_print_command_table(commands);
 	i = 0;
 	while (commands[i].command)
 	{
 		env->last_result = pc_exec_command(commands[i++], env);
+	}
+	i = 0;
+	while (commands[i].command)
+	{
+		close(commands[i].fd[0]);
+		close(commands[i].fd[1]);
+		i++;
 	}
 	return (env->last_result);
 }
