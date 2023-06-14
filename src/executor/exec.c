6847d834656120d87b7d308ff78583764960c107
Claudius J <digital.k.jack@gmail.com>
2023-06-14 12:14:53 +0200

    simple > work

--- src/executor/exec.c
+++ src/executor/exec.c
@@ -3,34 +3,68 @@
 /*                                                        :::      ::::::::   */
 /*   exec.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kgebski <kgebski@student.42wolfsburg.de    +#+  +:+       +#+        */
+/*   By: cjackows <cjackows@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/09 13:23:32 by kgebski           #+#    #+#             */
-/*   Updated: 2023/06/13 18:08:37 by kgebski          ###   ########.fr       */
+/*   Updated: 2023/06/13 19:57:47 by cjackows         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+static void	pc_file_redirection_check(t_command *command, t_env *env);
+
 int	pc_exec_commands(t_command *commands, t_env *env)
 {
 	int	i;
+	int fd_tmp;
 
 	i = 0;
 	pc_print_command_table(commands);
-	while (commands[i].command)
-	{
-		if (pipe(commands[i].fd) < 0)
-		{
-			ft_putstr_fd("Filed to create a pipe\n", 2);
-		}
-	}
+	// while (commands[i].command)
+	// {
+	// 	if (pipe(commands[i].fd) < 0)
+	// 	{
+	// 		ft_putstr_fd("Filed to create a pipe\n", 2);
+	// 	}
+	// }
 	i = 0;
 	while (commands[i].command)
 	{
-		env->last_result = pc_exec_command(commands[i++], env);
+		fd_tmp = dup(1);
+		pc_file_redirection_check(&(commands[i]), env);
+		env->last_result = pc_exec_command(commands[i], env);
+		dup2(fd_tmp, 1);
+		i++;
 	}
 	return (env->last_result);
+}
+
+static void	pc_file_redirection_check(t_command *command, t_env *env)
+{
+	int i;
+	int fd;
+
+	i = 0;
+	(void)env;
+	while(command->arguments && command->arguments[i])
+	{
+		if(ft_strncmp(command->arguments[i], ">", 1) == 0)
+		{
+			if (ft_strncmp(command->arguments[i], ">>", 2) == 0)
+				fd = open(command->arguments[i + 1], O_WRONLY | O_CREAT | O_APPEND, 0777);
+			else
+				fd = open(command->arguments[i + 1], O_WRONLY | O_CREAT | O_TRUNC, 0777);
+			if (fd == -1)
+				return(ft_putstr_fd("Filed to handle a file\n", 2));
+			dup2(fd, 1);
+			free(command->arguments[i]);
+			free(command->arguments[i + 1]);
+			command->arguments[i] = 0;
+			close(fd);
+		}
+		i++;
+	}
 }
 
 int	pc_exec_command(t_command command, t_env *env)
